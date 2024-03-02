09743ab59d4ea06deacdc8066797889d410865fb
Were Samson Bruno <werefast2000@gmail.com>
2024-03-02 21:49:40 +0000

    create login route

---
+++ my-app/app/[auth]/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Signin } from "@/components/component/signin";
+
+export default function Login () {
+    return (
+        <div>
+           
+            <Signin />
+        </div>
+    );
+};
