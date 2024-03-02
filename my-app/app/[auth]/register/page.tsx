401f945cace1fd8bf3e75a4798be1ab06fe1c425
Were Samson Bruno <werefast2000@gmail.com>
2024-03-02 21:49:26 +0000

    create register route

---
+++ my-app/app/[auth]/register/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Signup } from "@/components/component/signup";
+
+export default function Register () {
+    return (
+        <div>
+            <Signup />
+        </div>
+    );
+};
