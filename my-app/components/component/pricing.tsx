9a29d46dce966b77228c9f2827c9aa0f81058107
sila <silakironji@gmail.com>
2024-03-06 20:43:40 +0000

    route ResourcePage

--- my-app/components/component/pricing.tsx
+++ my-app/components/component/pricing.tsx
@@ -22,7 +22,7 @@ export function Pricing() {
             <a className="text-gray-600 hover:text-gray-900" href="/pricingPage">
               Pricing
             </a>
-            <a className="text-gray-600 hover:text-gray-900" href="#">
+            <a className="text-gray-600 hover:text-gray-900" href="/resourcesPage">
               Resources
             </a>
             <a className="text-gray-600 hover:text-gray-900" href="#">
