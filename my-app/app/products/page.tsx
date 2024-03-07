a407831362b39ffdb5f1e44c0928f5dadc44da72
Were Samson Bruno <werefast2000@gmail.com>
2024-03-07 15:41:07 +0000

    create products page and import products component

---
+++ my-app/app/products/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Products } from "@/components/component/products";
+
+export default function productsPage (){
+    return (
+        <div>
+            <Products/>
+        </div>
+    );
+};
