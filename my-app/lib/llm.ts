1cd0cbb6240efdf44674b3e169d2fc718de51af8
sila <silakironji@gmail.com>
2024-03-15 23:12:45 +0000

    model name

--- my-app/lib/llm.ts
+++ my-app/lib/llm.ts
@@ -1,14 +1,14 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
 export const streamingModel = new ChatOpenAI({
-  modelName: "gpt-3.5-turbo-0125",
+  modelName: "gpt-3.5-turbo-1106",
   streaming: true,
   verbose: true,
   temperature: 0,
 });
 
 export const nonStreamingModel = new ChatOpenAI({
-  modelName: "gpt-3.5-turbo-0125",
+  modelName: "gpt-3.5-turbo-1106",
   verbose: true,
   temperature: 0,
 });
