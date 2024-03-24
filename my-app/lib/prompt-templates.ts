841fb8ac7752d9d971bc1ecb9ada6ebd563c9743
sila <silakironji@gmail.com>
2024-03-24 15:49:40 +0000

    updated prompt

--- my-app/lib/prompt-templates.ts
+++ my-app/lib/prompt-templates.ts
@@ -7,7 +7,7 @@ Follow Up Input: {question}
 Standalone question:`;
 
 // Actual question you ask the chat and send the response to client
-export const QA_TEMPLATE = `You are an enthusiastic Hotel Customer support Agent. Use the following pieces of context to answer the question at the end.
+export const QA_TEMPLATE = `You are an enthusiastic Hotel Customer support Agent for LaPalm Royal. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
