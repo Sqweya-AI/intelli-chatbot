d2701e267e46430a6a92dc0a72e21ad6148ae712
Were Samson Bruno <werefast2000@gmail.com>
2024-03-27 10:24:32 +0000

    modify elli to accomodate reservations

--- my-app/components/chat-preview.tsx
+++ my-app/components/chat-preview.tsx
@@ -50,9 +50,10 @@ export function ChatPreview() {
         title="Website Preview"
       ></iframe>
       {chatbotVisible && (
-        <div className="fixed bottom-0 right-0 z-10 p-4">
+        <div 
+        className="fixed bottom-10 right-0 z-10 p-4">
           {/* Your chatbot component */}
-          <div className="bg-white rounded-lg shadow-md p-4">
+          <div className="bg-white rounded-lg shadow-md p-4" >
             {/* Content of your chatbot */}
             <button onClick={toggleChatbot}>Open Elli</button>
           </div>
@@ -70,7 +71,9 @@ export function ChatPreview() {
       </div>
       </div>
       {!chatbotVisible && (
-        <div className="fixed bottom-0 right-0 z-20 p-4">
+        <div 
+        style={{ width: "420px", height: "700px" }}
+        className="fixed bottom-10 right-0 z-20 p-4">
           <ChatWindow />
           <button onClick={toggleChatbot}>Close Elli</button>
         </div>
