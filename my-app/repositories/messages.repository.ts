24d2fbf9818701c258859510bdfd589215342e8d
Were Samson Bruno <werefast2000@gmail.com>
2024-04-01 14:20:37 +0100

    modify messages.repository

--- my-app/repositories/messages.repository.ts
+++ my-app/repositories/messages.repository.ts
@@ -1,14 +1,10 @@
-import {MessageModel} from '@/models/message.model';
-import { Model } from 'mongoose';
+import { Message, MessageModel } from '@/models/message.model';
 
+export async function addMessage(userId: string, message: string, aiResponse: string): Promise<MessageModel> {
+  const newMessage = new Message({ userId, message, aiResponse });
+  return newMessage.save();
+}
 
-
-  export async function addMessage(userId: string, message: string, aiResponse: string): Promise<Model<Message>> {
-    const newMessage = new Model({ userId, message, aiResponse });
-    return newMessage.save();
-  }
-  
-  export async function getMessages(userId: string): Promise<Model<Message>[]> {
-    return Model.find({ userId }).sort({ createdAt: 1 }).exec();
-  }
-  
+export async function getMessages(userId: string): Promise<MessageModel[]> {
+  return Message.find({ userId }).sort({ createdAt: 1 }).exec();
+}
