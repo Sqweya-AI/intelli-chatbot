c1860fd143bd20d1cd4a7b9d861e4ca28fb2d31e
Were Samson Bruno <werefast2000@gmail.com>
2024-05-25 23:10:50 +0000

    change footer

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import { Home } from "@/components/component/home";
-import Privacy from "@/components/privacy";
+import TwoColumnFooter from "@/components/sections/footer";
 
 export default function Homepage() {
   const [showPrivacy, setShowPrivacy] = useState(false);
@@ -10,21 +10,7 @@ export default function Homepage() {
   return (
     <main className="min-h-screen p-4 bg-white">
       <Home />
-      <footer className="w-full bg-white-500">
-        <div className="container-fluid ">
-          <p className="text-sm text-center text-gray-900">
-            Need help? Get in touch with us.
-          </p>
-          <p className="mt-2 text-xs text-center text-gray-900">
-            <a href="/terms">Terms of Use</a> |
-            <a href="/privacy" >
-              Privacy Policy
-            </a>{"/privacy"}
-            | Data Protection Policy | Socials | © IntelliConcierge 2024
-          </p>
-        </div>
-      </footer>
-      {showPrivacy && <Privacy />}
+      <TwoColumnFooter />
     </main>
   );
 }
