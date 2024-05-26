3c1074ba9c30c98ec4141e5648f36d70a00c2590
Were Samson Bruno <werefast2000@gmail.com>
2024-05-26 22:24:28 +0000

    remove privacy import

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,9 +4,8 @@ import { useState } from "react";
 import { Home } from "@/components/component/home";
 import TwoColumnFooter from "@/components/sections/footer";
 
-export default function Homepage() {
-  const [showPrivacy, setShowPrivacy] = useState(false);
 
+export default function Homepage() {
   return (
     <main className="min-h-screen p-4 bg-white">
       <Home />
