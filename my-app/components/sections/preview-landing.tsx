653e2edb9e8b4a634352d5c9f1ae003122989deb
Were Samson Bruno <werefast2000@gmail.com>
2024-07-09 22:38:13 +0000

    change content of preview-landing page from img to video

--- my-app/components/sections/preview-landing.tsx
+++ my-app/components/sections/preview-landing.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image";
-import demoImage from "@/public/Demo.svg";
+import React from 'react';
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export function PreviewLanding() {
   return (
     <div className="pb-6 sm:pb-16">
       <div className="container max-w-7xl">
         <div className="rounded-xl bg-muted/30 p-4 ring-1 ring-inset ring-border">
-          <div className="relative aspect-video overflow-hidden rounded-lg border">
-            <Image
-              className="size-full object-cover object-center"
-              
-              alt="preview of dashboard"
-              priority={true}
-              src={demoImage}
-            />
-          </div>
+          <AspectRatio ratio={16 / 9} className="overflow-hidden rounded-lg">
+            <iframe
+              className="w-full h-full"
+              src="https://www.youtube.com/embed/Q_EYoV1kZWk?si=D3vDj0BKmVDPbxo5"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </AspectRatio>
         </div>
       </div>
     </div>
