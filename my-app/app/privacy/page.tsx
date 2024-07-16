e3804b25c46d9ffef923788d47496dd8984666fa
Were Samson Bruno <werefast2000@gmail.com>
2024-07-16 23:37:45 +0000

    add navbar to privacy policy page

--- my-app/app/privacy/page.tsx
+++ my-app/app/privacy/page.tsx
@@ -1,4 +1,5 @@
 import PrivacyPolicy from "@/components/privacy";
+import { Navbar } from "@/components/navbar";
 import React from "react";
 // import { Separator } from "@/components/ui/separator";
 
@@ -8,6 +9,7 @@ export default function Privacy() {
   return (
     <div className="relative">
     <main className="pt-16">
+    <Navbar />
         <section className="container mx-auto mt-8 px-4 lg:2/4 xl:w-2/3 ml-22.5 sm:w-3/4">
         <PrivacyPolicy /> 
         </section>
