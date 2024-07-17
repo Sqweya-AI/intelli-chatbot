c3811338ddb398f10eb793a27eb16e827a1fab33
Were Samson Bruno <werefast2000@gmail.com>
2024-07-17 23:00:39 +0000

    modify middleware to add route protection to sensitive routes

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,7 +1,33 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Define routes that should be protected
+const isProtectedRoute = createRouteMatcher([
+  '/demo',
+  '/chat',
+  '/dashboard',
+  '/dashboard/create-chatbot',
+  '/dashboard/reservations',
+  '/dashboard/billing',
+  '/dashboard/settings',
+  '/dashboard/employees',
+ '/dashboard/conversations',
+  '/dashboard/conversations/elli',  
+  '/dashboard/conversations/email',
+  '/dashboard/conversations/voice-assistant',
+  '/dashboard/conversations/whatsapp',
+  '/dashboard/conversations/whatsapp/chat_sessions',
+  '/dashboard/conversations/whatsapp/chat_sessions/:phoneNumber',
+  '/dashboard/conversations/whatsapp/chat_sessions/:phoneNumber/:recipientId',
+  '/dashboard/conversations/whatsapp/chat_sessions/:phoneNumber/:recipientId/messages',
+  // Add any additional routes here
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect(); // Protect the route if it matches the defined criteria
+  }
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
