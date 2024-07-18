52bbac9b4ffa96de3c7b4fcf2f4454e8b0755f93
Were Samson Bruno <werefast2000@gmail.com>
2024-07-18 11:27:35 +0000

    remove commented out code

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,9 +1,5 @@
 "use client"
-
-// import { useRouter } from 'next/navigation';
-// import { useLayoutEffect } from 'react';
 import { DashComponent } from "@/components/component/dash";
-// import useAuth from '@/lib/auth/useAuth';
 
 export default function Page() {
 
