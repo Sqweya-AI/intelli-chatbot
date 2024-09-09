79bf9c1a6e22971c0bc8b689a6d426b44631f788
Were Samson Bruno <werefast2000@gmail.com>
2024-09-09 22:38:51 +0300

    create waitlist page

---
+++ my-app/app/join-waitlist/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Waitlist from "@/components/component/waitlist";
+import { Navbar } from "@/components/navbar";
+import React from "react";
+import WhatsAppSignup from "@/components/component/whatsappSignup";
+
+export default function Company() {
+  return (
+    <div className="relative">
+      <Navbar />
+      <main className="pt-16">
+        <section className="container mx-auto mt-8 px-4 lg:2/4 xl:w-2/3 ml-22.5 sm:w-3/4">
+          <h1 className="text-5xl font-bold text-center text-left text-blue-600 mb-10">
+          Join Our Waitlist & Become An Early-adopter
+          </h1>
+          
+          <Waitlist />
+        </section>
+
+        
+        
+        <WhatsAppSignup />
+      </main>
+    </div>
+  );
+}
