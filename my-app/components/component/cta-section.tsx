d03beee7058b1b635c9608acc26daaa11f4cb200
Were Samson Bruno <werefast2000@gmail.com>
2024-09-09 22:39:06 +0300

    modify cta section

--- my-app/components/component/cta-section.tsx
+++ my-app/components/component/cta-section.tsx
@@ -8,17 +8,17 @@ const CTASection = () => {
       <div className="flex flex-col md:flex-row items-center justify-between gap-8">
         <div className="md:w-1/2">
           <div className="rounded-lg h-auto overflow-hidden bg-lavender-100 shadow-md border border-transparent hover:border-blue-500 focus-within:border-blue-500">
-            <Waitlist />
+            
           </div>
         </div>
         <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-4xl font-bold mb-4 text-gray-800">
-            Give Your Customer Service An Upgrade.
+            Upgrade Your Business Today
           </h2>
           <p className="text-lg text-gray-700 mb-6">
-            Become a market leader by joining our exclusive waitlist. Gain early access to our products and stay ahead of competitors and the market.
+            Join the waitlist to get early access to our platform and a 20% discount for your business.
           </p>
-          <Badge variant="destructive" className="text-lg p-2">
+          <Badge variant="destructive" className="text-lg rounded-xl p-2">
             20% Discount for Businesses on Waitlist!
           </Badge>
         </div>
