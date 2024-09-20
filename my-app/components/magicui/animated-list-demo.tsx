3cd0f6aa78e0b5388b4952332cf084a8f91d701e
Were Samson Bruno <werefast2000@gmail.com>
2024-09-20 15:27:27 +0300

    Refactor notification descriptions in AnimatedBeamDemo component

--- my-app/components/magicui/animated-list-demo.tsx
+++ my-app/components/magicui/animated-list-demo.tsx
@@ -14,29 +14,29 @@ interface Item {
 let notifications = [
   {
     name: "Payment received",
-    description: "Magic UI",
+    description: "Whatsapp Assistant.",
     time: "15m ago",
 
     icon: "💸",
     color: "#00C9A7",
   },
   {
-    name: "User signed up",
-    description: "Magic UI",
+    name: "Inquiry about your services.",
+    description: "Whatsapp Assistant",
     time: "10m ago",
     icon: "👤",
     color: "#FFB800",
   },
   {
-    name: "New message",
-    description: "Magic UI",
+    name: "Client asked for catalogue.",
+    description: "Instagram Assistant",
     time: "5m ago",
     icon: "💬",
     color: "#FF3D71",
   },
   {
-    name: "New event",
-    description: "Magic UI",
+    name: "Client asked for a human.",
+    description: "Website Widget",
     time: "2m ago",
     icon: "🗞️",
     color: "#1E86FF",
@@ -54,8 +54,7 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
         "transition-all duration-200 ease-in-out hover:scale-[103%]",
         // light styles
         "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
-        // dark styles
-        "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+       
       )}
     >
       <div className="flex flex-row items-center gap-3">
@@ -68,12 +67,12 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
           <span className="text-lg">{icon}</span>
         </div>
         <div className="flex flex-col overflow-hidden">
-          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
+          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium  ">
             <span className="text-sm sm:text-lg">{name}</span>
             <span className="mx-1">·</span>
             <span className="text-xs text-gray-500">{time}</span>
           </figcaption>
-          <p className="text-sm font-normal dark:text-white/60">
+          <p className="text-sm font-normal text-grey/60">
             {description}
           </p>
         </div>
