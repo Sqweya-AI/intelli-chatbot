32829bd96d4a655d43132e9195e21f6a60654d61
Were Samson Bruno <werefast2000@gmail.com>
2024-10-01 01:24:25 +0300

    render medium blog component

---
+++ my-app/app/medium-blog/page.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/navbar";
+import React from "react";
+import MediumBlogComponent from "./medium-blog-component";
+
+export default function mediumBlogPage() {
+  return (
+    <div>
+      <Navbar />
+      <MediumBlogComponent />
+    </div>
+  );
+}
