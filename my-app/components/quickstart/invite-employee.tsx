82562a823578a662cd08364ea68d3a818d8bd08e
Were Samson Bruno <werefast2000@gmail.com>
2024-10-07 13:55:27 +0300

    renamed invite dev to invite employee

--- my-app/components/quickstart/invite-employee.tsx
+++ my-app/components/quickstart/invite-employee.tsx
@@ -4,19 +4,19 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
-export default function InviteDeveloper() {
+export default function InviteEmployee() {
   return (
     <div className="space-y-4">
       <p className="text-sm text-gray-500">
-        Integrating Liveblocks takes a few minutes but front-end coding skills are required. 
-        Invite a developer to your team to help set things up.
+        Invite a team member or employee to gain access.
       </p>
       <div className="space-y-2">
-        <Label htmlFor="email">Developer&apos;s email</Label>
+        <Label htmlFor="email">Employee&apos;s email</Label>
         <div className="flex space-x-2">
           <Input id="email" placeholder="Enter email address" />
-          <Button>Invite</Button>
+          
         </div>
+        <Button>Send Invite</Button>
       </div>
     </div>
   );
