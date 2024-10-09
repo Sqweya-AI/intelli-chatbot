a38ad438be3710b0c4a992afb5e6b42df0f06ff2
Were Samson Bruno <werefast2000@gmail.com>
2024-10-09 18:05:42 +0300

    remove container from div

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import { Suspense } from 'react';
 export default function Page() {
 
   return (
-    <div className="container mx-auto p-6">
+    <div className=" mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
       <Suspense fallback={<Skeleton className="w-full h-[600px]" />}>
       <DashComponent />
