e62b6a962442ee819b7b72a1e385a4995f72b8ed
Were Samson Bruno <werefast2000@gmail.com>
2024-10-09 18:00:41 +0300

    added Get Started button to dashboard

--- my-app/components/component/dash.tsx
+++ my-app/components/component/dash.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { MessageSquare, Users, CalendarCheck, Activity, Filter, BarChart as BarChartIcon, LineChart as LineChartIcon, PieChartIcon } from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -11,12 +13,17 @@ import { StatsOverview } from '@/components/dash-components/stats';
 export function DashComponent() {
 
   return (
-    <div className="p-6 space-y-6">
+    <div className="p-1 space-y-6">
       <div className="flex justify-between items-center">
+      <Link
+              href={ "/dashboard/get-started" }
+              onClick={() => {
+              }}
+            >
         <div>
-          <Button variant="outline" className="mr-2">Overview</Button>
-          
+          <Button variant="default" className="mr-2 bg-blue-600">Get Started</Button>          
         </div>
+        </Link>
       </div>
 
       <StatsOverview />
