63fbc431cf46ff2676f17a462754ffc1a376f9a9
Were Samson Bruno <werefast2000@gmail.com>
2024-10-09 18:02:47 +0300

    import Pricing Component from pricing

--- my-app/app/pricing/page.tsx
+++ my-app/app/pricing/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {Pricing} from "@/components/component/pricing";
+import PricingComponent from "@/components/component/pricingcomponent";
 
 export default function pricingPage (){
     return (
         <div>
-            <Pricing/>
+            <PricingComponent/>
         </div>
     );
 };
