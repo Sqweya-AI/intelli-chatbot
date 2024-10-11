6b073adda2f2ffb70188119d04bec8502e230038
Were Samson Bruno <werefast2000@gmail.com>
2024-10-11 14:31:51 +0300

    add navbar to pricing page

--- my-app/app/pricing/page.tsx
+++ my-app/app/pricing/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Pricing} from "@/components/component/pricing";
 import PricingComponent from "@/components/component/pricingcomponent";
+import { Navbar } from "@/components/navbar";
 
 export default function pricingPage (){
     return (
         <div>
+            <Navbar />
             <PricingComponent/>
         </div>
     );
