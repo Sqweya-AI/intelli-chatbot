7b52f8ba39ec87b172b4ecc6ff897ebe38054d64
Were Samson Bruno <werefast2000@gmail.com>
2024-10-17 17:36:08 +0300

    add custom card: Onborda

---
+++ my-app/components/CustomCard/index.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React from "react";
+import type { CardComponentProps } from "onborda";
+import { useOnborda } from "onborda";
+import { XIcon } from "lucide-react";
+
+import confetti from "canvas-confetti";
+
+// Shadcn
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const CustomCard: React.FC<CardComponentProps> = ({
+  step,
+  currentStep,
+  totalSteps,
+  nextStep,
+  prevStep,
+  arrow,
+}) => {
+  // Onborda hooks
+  const { closeOnborda } = useOnborda();
+
+  function handleConfetti() {
+    closeOnborda();
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
+    });
+  }
+
+  return (
+    <Card className="border-0 rounded-3xl max-w-vw">
+      <CardHeader>
+        <div className="flex items-start justify-between w-full">
+          <div>
+            <CardTitle className="mb-2 text-lg">
+              {step.icon} {step.title}
+            </CardTitle>
+            <CardDescription>
+              {currentStep + 1} of {totalSteps}
+            </CardDescription>
+          </div>
+          <Button variant="ghost" size="default" onClick={() => closeOnborda()}>
+            <XIcon size={16} />
+          </Button>
+        </div>
+      </CardHeader>
+      <CardContent>{step.content}</CardContent>
+      <CardFooter>
+        <div className="flex justify-between w-full">
+          {currentStep !== 0 && (
+            <Button onClick={() => prevStep()}>Previous</Button>
+          )}
+          {currentStep + 1 !== totalSteps && (
+            <Button onClick={() => nextStep()} className="ml-auto">
+              Next
+            </Button>
+          )}
+          {currentStep + 1 === totalSteps && (
+            <Button onClick={() => handleConfetti()} className="ml-auto">
+              🎉 Finish!
+            </Button>
+          )}
+        </div>
+      </CardFooter>
+      <span className="text-card">{arrow}</span>
+    </Card>
+  );
+};
+
+export default CustomCard;
