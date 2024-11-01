915c3a52fe6c71f10ffaae223615a297f36e75e2
Were Samson Bruno <werefast2000@gmail.com>
2024-11-01 16:55:24 +0300

    change command center to home

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
 
   return (
     <div className=" container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6">Command Center</h1>
+      <h1 className="text-2xl font-bold mb-6">Home</h1>
       <Suspense fallback={<Skeleton className="w-full h-[600px]" />}>
       <DashComponent />
       </Suspense>      
