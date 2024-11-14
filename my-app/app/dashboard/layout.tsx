31d533f40ddffaa781babc821fbdddd30a8aaf51
Were Samson Bruno <werefast2000@gmail.com>
2024-11-14 23:55:28 +0300

    removed unused imports + added new sidebar

--- my-app/app/dashboard/layout.tsx
+++ my-app/app/dashboard/layout.tsx
@@ -1,16 +1,12 @@
 import Header from "@/components/layout/header";
-import Sidebar from "@/components/layout/sidebar";
 import type { Metadata } from "next";
 import { Toaster } from "sonner";
-
-// Onborda
-import { Onborda, OnbordaProvider } from "onborda";
-import { steps } from "@/lib/steps";
-
-
-// Custom Card
-import CustomCard from "@/components/CustomCard";
-
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export const metadata: Metadata = {
   title: "Intelli Dashboard",
@@ -30,16 +26,25 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div suppressHydrationWarning>     
-     <Toaster position="top-right" />
-      <Header />
-      <div className="flex h-screen">
-        <Sidebar />
-        <main className="w-full pt-16">        
-          {children}       
-          </main>
-      </div>
-      
+    <div suppressHydrationWarning>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "17rem",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />          
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <Toaster position="top-right" />
+            <main className="">{children}</main>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </div>
   );
 }
