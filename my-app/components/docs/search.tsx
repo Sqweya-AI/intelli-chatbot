7e80fdffa446c8601b29836a10efa430941b7e9c
Were Samson Bruno <werefast2000@gmail.com>
2024-12-15 12:51:12 +0300

    modify copy in search component

--- my-app/components/docs/search.tsx
+++ my-app/components/docs/search.tsx
@@ -4,11 +4,11 @@ import { Search as SearchIcon } from "lucide-react"; // Changed to correct impor
 
 export const Search: React.FC = () => {
   return (
-    <div className="relative max-w-md w-full border-white-100 border-rounded-lg shadow-sm">
-      <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+    <div className="relative max-w-md w-full border-white-100 border-rounded-lg ">
+      <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-blue-600 w-5 h-5" />
       <Input
         type="search"
-        placeholder="Search documentation..."
+        placeholder="Search..."
         className="pl-10"
       />
     </div>
